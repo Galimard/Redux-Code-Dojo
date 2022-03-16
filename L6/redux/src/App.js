b963a26d788323cdb3df821873bfc90ee826be14
Galimard <galimard.maria@gmail.com>
2022-03-16 13:37:49 +0800

    action creators

--- L6/redux/src/App.js
+++ L6/redux/src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { ReactDOM } from 'react';
 import Counter from './Counter';
 import Counter2 from './Counter2';
+import Counter3 from './Counter3';
 
 function App() {
   return (
-    <Counter2 />
+    <Counter3 />
   );
 }
 
